0d772ef2c7db8c17e193388ec316baa90f14ef9e
caetanodog99 <caetanouniso@gmail.com>
2026-05-26 21:59:06 -0300

    ragdoll physics v1

---
+++ ragdoll/Assets/scripts/AnimationRagdoll.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationRagdoll : MonoBehaviour
+{
+    [SerializeField] private Collider myCollider;
+    [SerializeField] private float respawnTime = 5f;
+    Rigidbody[] rigidbodies;
+    private bool isRagdoll = false;
+   
+
+    void Start()
+    {
+        rigidbodies = GetComponentsInChildren<Rigidbody>();
+        ToggleRagdoll(true);
+        transform.position = new Vector3(0, 0, 0) * 5 * Time.deltaTime;
+    }
+
+    private void ToggleRagdoll(bool isAnimation)
+    {
+        isRagdoll = !isAnimation;
+        myCollider.enabled = isAnimation;
+
+        foreach (Rigidbody ragdollBody in rigidbodies)
+        {
+            //ragdollBody.isKinematic = isAnimation;
+
+            GetComponent<Animator>().enabled = isAnimation;
+
+            if (isAnimation) RandomAnimation();
+        }
+    } 
+
+    private void RandomAnimation()
+    {
+        int randomNum = UnityEngine.Random.Range(0, 2);
+
+        Animator animator = GetComponent<Animator>();
+
+        if (randomNum == 0)
+        {
+            animator.SetTrigger("walk");
+        }
+        else animator.SetTrigger("idle");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("NPC") && !isRagdoll)
+        {
+            ToggleRagdoll(false);
+            StartCoroutine(GetBackup());
+        }
+    }
+
+    private IEnumerator GetBackup()
+    {
+        yield return new WaitForSeconds(respawnTime);
+        ToggleRagdoll(true);
+    }
+    void Update()
+    {
+
+    }
+}
